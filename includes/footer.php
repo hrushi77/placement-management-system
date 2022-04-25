e9926234160992a439e79500e3c8197608d3be9b
hrushikesh <hrushi@.com>
2022-04-26 02:34:23 +0530

    third commit

---
+++ includes/footer.php
@@ -0,0 +1,51 @@
+<!--/footer-->
+<div class="footer">
+    <div class="footer-top">
+        <div class="col-md-4 footer-grid">
+            <h4>PLACEMENT Management System</h4>
+            <ul class="bottom">
+                <li>Designed By: Hrushikesh Mahesh Sawant, Mohammed Zaid Qureshi, Ashwin Suresh Tiwari</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Message Us Now</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-home"></i>Available 24/7 </li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="hrushikesh@gmail.com">hrushikesh@gmail.com</a></li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="zaid@gmail.com">zaid@gmail.com</a></li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="ashwin@gmail.com">ashwin@gmail.com</a></li>
+                <li><i class="glyphicon glyphicon-envelope"></i>9272777334</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Address Location</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-map-marker"></i>Carter Road, Bandra West.</li>
+                <li><i class="glyphicon glyphicon-earphone"></i>Mobile: 9727773349 </li>
+            </ul>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="copy">
+    <p>&copy; 2021 Placement Management System All Rights Reserved</p>
+</div>
+<div class="clearfix"> </div>
+<!--//footer-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    });
+</script>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
